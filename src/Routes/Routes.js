c8882816d760a9cf1eee4f649eb87b8f094bc306
Shekh Rasel Masrur Ahmmad Nissan <shekhraselmasrurahmmadnissan@gmail.com>
2022-11-08 22:14:56 +0600

    Added the Error page to the Routes.

--- src/Routes/Routes.js
+++ src/Routes/Routes.js
@@ -1,5 +1,6 @@
 import { createBrowserRouter } from 'react-router-dom';
 import MainLayout from '../Layouts/MainLayout';
+import Error from '../Pages/Error/Error';
 import Home from '../Pages/Home/Home';
 import Login from '../Pages/Login/Login';
 import Register from '../Pages/Register/Register';
@@ -8,6 +9,7 @@ const router = createBrowserRouter([
 	{
 		path: '/',
 		element: <MainLayout />,
+		errorElement: <Error />,
 		children: [
 			{ index: true, element: <Home /> },
 			{ path: 'home', element: <Home /> },
