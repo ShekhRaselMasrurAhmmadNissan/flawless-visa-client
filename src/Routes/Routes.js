d1ee7203c289b95ca7e07543179387867efe7eac
Shekh Rasel Masrur Ahmmad Nissan <shekhraselmasrurahmmadnissan@gmail.com>
2022-11-08 15:53:52 +0600

    Created Routes.

---
+++ src/Routes/Routes.js
@@ -0,0 +1,6 @@
+import { createBrowserRouter } from 'react-router-dom';
+import MainLayout from '../Layouts/MainLayout';
+
+const router = createBrowserRouter([{ path: '/', element: <MainLayout /> }]);
+
+export default router;
