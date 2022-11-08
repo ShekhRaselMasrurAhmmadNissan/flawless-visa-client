acc54b0e18e7c4156f12d146b1e44ae6dae046ab
Shekh Rasel Masrur Ahmmad Nissan <shekhraselmasrurahmmadnissan@gmail.com>
2022-11-08 18:58:50 +0600

    Added Home, Login and Register path to the Routes.

--- src/Routes/Routes.js
+++ src/Routes/Routes.js
@@ -1,6 +1,20 @@
 import { createBrowserRouter } from 'react-router-dom';
 import MainLayout from '../Layouts/MainLayout';
+import Home from '../Pages/Home/Home';
+import Login from '../Pages/Login/Login';
+import Register from '../Pages/Register/Register';
 
-const router = createBrowserRouter([{ path: '/', element: <MainLayout /> }]);
+const router = createBrowserRouter([
+	{
+		path: '/',
+		element: <MainLayout />,
+		children: [
+			{ index: true, element: <Home /> },
+			{ path: 'home', element: <Home /> },
+			{ path: 'login', element: <Login /> },
+			{ path: 'register', element: <Register /> },
+		],
+	},
+]);
 
 export default router;
