743f030a45a60f82b7a4694e9918ceaa8d64c312
Shekh Rasel Masrur Ahmmad Nissan <shekhraselmasrurahmmadnissan@gmail.com>
2022-11-08 21:52:04 +0600

    Created Auth Provider and login, register and Logout functionality.

---
+++ src/Contexts/AuthProvider/AuthProvider.js
@@ -0,0 +1,46 @@
+import React, { createContext, useEffect, useState } from 'react';
+import {
+	createUserWithEmailAndPassword,
+	getAuth,
+	onAuthStateChanged,
+	signInWithEmailAndPassword,
+	signOut,
+} from 'firebase/auth';
+import app from '../../Firebase/Firebase.config';
+
+export const AuthContext = createContext();
+
+const auth = getAuth(app);
+const AuthProvider = ({ children }) => {
+	const [user, setUser] = useState(null);
+	const [loading, setLoading] = useState(true);
+
+	const emailRegister = (email, password) => {
+		return createUserWithEmailAndPassword(auth, email, password);
+	};
+
+	const login = (email, password) => {
+		return signInWithEmailAndPassword(auth, email, password);
+	};
+
+	const logout = () => {
+		return signOut(auth);
+	};
+
+	useEffect(() => {
+		const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+			setUser(currentUser);
+			setLoading(false);
+
+			return () => {
+				unsubscribe();
+			};
+		});
+	}, []);
+	const authInfo = { user, loading, emailRegister, login, logout };
+	return (
+		<AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
+	);
+};
+
+export default AuthProvider;
