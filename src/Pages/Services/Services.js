83a4717c7be6d5b58273c56293195b4240cf91b9
Shekh Rasel Masrur Ahmmad Nissan <shekhraselmasrurahmmadnissan@gmail.com>
2022-11-09 20:29:26 +0600

    Used Loader to load data in the Services Page.

--- src/Pages/Services/Services.js
+++ src/Pages/Services/Services.js
@@ -1,23 +1,12 @@
-import axios from 'axios';
-import React, { useEffect, useState } from 'react';
+import React from 'react';
+import { useLoaderData } from 'react-router-dom';
 import SingleService from '../../Components/SingleService/SingleService';
 
 const Services = () => {
-	const [services, setServices] = useState([]);
-
-	useEffect(() => {
-		const loadServices = async () => {
-			const response = await axios.get(`http://localhost:5000/services`);
-			// console.log(response);
-			setServices(response.data);
-		};
-		loadServices().catch((error) =>
-			console.error(error.message, error.name, error.stack)
-		);
-	}, []);
+	const services = useLoaderData();
 
 	return (
-		<div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 md:gap-3 mt-8">
+		<div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 md:gap-3 md:gap-y-6 mt-8">
 			{services.map((service) => (
 				<SingleService key={service._id} service={service} />
 			))}
