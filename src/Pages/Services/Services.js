aa57dbe858b2b04323f617748f7152a9611b2d18
Shekh Rasel Masrur Ahmmad Nissan <shekhraselmasrurahmmadnissan@gmail.com>
2022-11-10 02:57:33 +0600

    Edited Services Section.

--- src/Pages/Services/Services.js
+++ src/Pages/Services/Services.js
@@ -6,10 +6,15 @@ const Services = () => {
 	const services = useLoaderData();
 
 	return (
-		<div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 md:gap-3 md:gap-y-6 mt-8">
-			{services.map((service) => (
-				<SingleService key={service._id} service={service} />
-			))}
+		<div className="mt-4">
+			<h1 className="text-6xl font-medium text-center text-blue-600">
+				Services
+			</h1>
+			<div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 md:gap-3 md:gap-y-6 mt-8">
+				{services.map((service) => (
+					<SingleService key={service._id} service={service} />
+				))}
+			</div>
 		</div>
 	);
 };
