9b3b36b569c0c995a2cffc7febb2168c1f05cb15
Shekh Rasel Masrur Ahmmad Nissan <shekhraselmasrurahmmadnissan@gmail.com>
2022-11-09 11:41:38 +0600

    Added Advertise and Why us Section to the Home page.

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import Advertise from '../../Components/Advertise/Advertise';
 import Header from '../../Components/Header/Header';
+import WhyUs from '../../Components/WhyUs/WhyUs';
 
 const Home = () => {
 	return (
 		<div>
 			<Header />
+			<Advertise />
+			<WhyUs />
 		</div>
 	);
 };
